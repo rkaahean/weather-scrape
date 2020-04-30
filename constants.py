6224f7ca3f5cac95bbeeb691833ec56b1df35f56
rkaahean <ranjan31051997@gmail.com>
2020-04-29 18:33:27 -0700

    Finished basic setup for scraping.

--- constants.py
+++ constants.py
@@ -1,1 +1,1 @@
-
+API_CITY_FORECAST = 'api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}'
--- scrape.py
+++ scrape.py
@@ -1,14 +1,30 @@
 import requests
 from datetime import date
 import logging
-from constants import API_KEY
-
+import configparser
+"""
+Setting up logging.
+"""
 sc_log = logging.getLogger(__name__)
 sc_log.setLevel(logging.DEBUG)
 
 day = date.today()
 formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
 
-handler = logging.FileHandler('logs/' + str(day))
+handler = logging.FileHandler('logs/' + str(day) + '.log')
+sc_log.addHandler(handler)
 
-print(requests.__version__)
+"""
+Loading in the Key.
+"""
+sc_log.log(logging.DEBUG, "Getting API-KEY")
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+try:
+    API_KEY = config['KEY']['API-KEY']
+except KeyError:
+    sc_log.exception("Please define the key in the 'KEY' section, with 'API-KEY' key.")
+
+
+
