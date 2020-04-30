1e3690ba6531b560f02b77bce86f31893c1e8f86
rkaahean <ranjan31051997@gmail.com>
2020-04-29 17:58:31 -0700

    Initializing logging functionalities.

--- scrape.py
+++ scrape.py
@@ -1,4 +1,14 @@
 import requests
+from datetime import date
+import logging
+from constants import API_KEY
 
-print("Hi.")
+sc_log = logging.getLogger(__name__)
+sc_log.setLevel(logging.DEBUG)
+
+day = date.today()
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+handler = logging.FileHandler('logs/' + str(day))
+
 print(requests.__version__)
