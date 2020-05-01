5201159c9649d549b078836660f0f20973d625f0
rkaahean <ranjan31051997@gmail.com>
2020-05-01 01:27:02 -0700

    Added tentative list of cities.

--- constants.py
+++ constants.py
@@ -1,1 +1,13 @@
-API_CITY_FORECAST = 'api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}'
+API_CITY_FORECAST = 'api.openweathermap.org/data/2.5/forecast?q={}&appid={}'
+
+CITY_LIST = [
+    'Mumbai',
+    'New York',
+    'San Francisco',
+    'Los Angeles',
+    'Austin',
+    'Hyderabad',
+    'Muscat',
+    'Dubai',
+    'Chennai'
+]
